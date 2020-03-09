59e30b5d139eb0622bf6250b36f2c084addb80c1
epikgr <k.grabiner@epiconcept.fr>
2020-03-09 12:01:57 +0100

    commit après 2 heurs

--- api/v1/file/confidential/index.php
+++ api/v1/file/confidential/index.php
@@ -1,4 +1,64 @@
 <?php
-include_once  dirname(__FILE__) . '/../../../../objects/managers/FileManager.php';
+include_once dirname( __FILE__ ) . '/../../../../objects/managers/FileManager.php';
 
+/**
+ * @return File
+ */
+function initFileObject() {
+    $sId = null;
+    if ( isset( $_GET[ 'id' ] ) ) {
+        $sId = $_GET[ 'id' ];
+    };
 
+    $sContent = null;
+    if ( isset( $_FILES[ 'file' ][ 'tmp_name' ] ) ) {
+        $sContent = file_get_contents( $_FILES[ 'file' ][ 'tmp_name' ] );
+    };
+    return new File( array(
+        'id' => $sId,
+        'content' => $sContent
+    ) );
+}
+
+$oFile = initFileObject();
+
+$sHTTPVerb = $_SERVER[ 'REQUEST_METHOD' ];
+$oFileManager = new FileManager( $oFile );
+try {
+    switch ( $sHTTPVerb ) {
+        case 'POST':
+            $sNewId = $oFileManager->create();
+            _success( array( 'id' => $sNewId ) );
+            break;
+    }
+} catch ( Exception $e ) {
+    _failed( $e );
+}
+
+//
+//$oFileManager = new FileManager( array( 'file' => $oFile ) );
+//
+//$sId = $oFileManager->create();
+
+echo $sHTTPVerb;
+
+function _success( $aResponse ) {
+    http_response_code( 200 );
+
+    $aJsonReturn = array_merge( array(
+        'success' => true
+    ), $aResponse );
+    echo json_encode( $aJsonReturn );
+}
+
+function _failed( Exception $oException ) {
+    http_response_code( 500 );
+
+    $sMessage = $oException->getMessage();
+
+    $aJsonReturn = array(
+        'success' => false,
+        'message' => $sMessage
+    );
+    echo json_encode( $aJsonReturn );
+}
--- objects/managers/FileManager.php
+++ objects/managers/FileManager.php
@@ -1,18 +1,44 @@
 <?php
+include_once dirname( __FILE__ ) . '/../BaseObject.php';
 include_once dirname( __FILE__ ) . '/../File.php';
 
-class FileManager {
+class FileManager extends BaseObject {
+    /** @var File */
+    private $_oFile;
 
+    /**
+     * @return File
+     */
+    public function getFile() {
+        return $this->_oFile;
+    }
 
-    public function create( $sContent ) {
+    /**
+     * @param File $oFile
+     */
+    public function setFile( $oFile ) {
+        $this->_oFile = $oFile;
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    public function create() {
+        if( $this->getFile()->getContent() === null ) {
+            throw new Exception( 'The contents of the file must not be empty' );
+        }
+        $sId = $this->_createUniqId();
+        $rFile = fopen( $this->_getUploadsDir() . $sId, 'c+b' );
+        fwrite( $rFile, $this->getFile()->getContent() );
+        return $sId;
+    }
+
+    public function delete() {
 
     }
 
-    public function delete( $sId ) {
-
-    }
-
-    public function get( $sId ) {
+    public function get() {
 
     }
 
@@ -22,5 +48,12 @@ class FileManager {
 
     private function _createUniqId() {
         return uniqid( 'file_' );
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getUploadsDir() {
+        return dirname( __FILE__ ) . '/../../uploads/';
     }
 }
